218103a20cc5e91cff2ea029f052293eb82a474b
Luciano Iocco <liocco@terpmail.umd.edu>
2025-06-27 14:22:20 -0400

    Uploading beginning stages

--- server/Lib.cs
+++ server/Lib.cs
@@ -1,4 +1,5 @@
 using SpacetimeDB;
+using SpacetimeDB.Internal.TableHandles;
 
 public static partial class Module
 {
@@ -9,20 +10,25 @@ public static partial class Module
     public partial class User
     {
         [PrimaryKey]
-        public Identity Identity;
+        public Identity Client;
         public string? Name;
         public bool Online;
+        public bool isActive;
     }
 
     /// <summary>
     /// This class is used to store the Identity of the user who sent the message, the Timestamp when it was sent, and the message text.
     /// </summary>
-    [Table(Name = "message", Public = true)]
-    public partial class Message
+    [Table(Name = "cursor", Public = true)]
+    public partial class Cursor
     {
-        public Identity Sender;
-        public Timestamp Sent;
-        public string Text = "";
+        [Unique]
+        public Identity Client;
+        public float X;
+        public float Y;
+        public Timestamp LastUpdated;
+
+
     }
 
     [Reducer]
@@ -30,11 +36,11 @@ public static partial class Module
     {
         name = ValidateName(name);
 
-        var user = ctx.Db.user.Identity.Find(ctx.Sender);
+        var user = ctx.Db.user.Client.Find(ctx.Sender);
         if (user is not null)
         {
             user.Name = name;
-            ctx.Db.user.Identity.Update(user);
+            ctx.Db.user.Client.Update(user);
         }
     }
 
@@ -49,87 +55,55 @@ public static partial class Module
     }
 
     /// <summary>
-    /// A client will call this reducer to send messages. It validates the text, then inserts a new Message record with the sender, identity, and time. 
-    /// </summary>
-    /// <param name="ctx"></param>
-    /// <param name="text"></param>
-    [Reducer]
-    public static void SendMessage(ReducerContext ctx, string text)
-    {
-        text = ValidateMessage(text);
-        Log.Info(text);
-        ctx.Db.message.Insert(
-            new Message
-            {
-                Sender = ctx.Sender,
-                Text = text,
-                Sent = ctx.Timestamp,
-            }
-        );
-    }
-
-    /// Takes a message's text and checks if it's acceptable to send.
-    private static string ValidateMessage(string text)
-    {
-        if (string.IsNullOrEmpty(text))
-        {
-            throw new ArgumentException("Messages must not be empty");
-        }
-        return text;
-    }
-
-    /// <summary>
     /// This will update the client's online status once they connect to the server
     /// </summary>
     /// <param name="ctx"></param>
     [Reducer(ReducerKind.ClientConnected)]
-    public static void ClientConnected(ReducerContext ctx)
+    public static void UserConnected(ReducerContext ctx)
     {
         Log.Info($"Connect {ctx.Sender}");
-        var user = ctx.Db.user.Identity.Find(ctx.Sender);
+        var user = ctx.Db.user.Client.Find(ctx.Sender);
 
         if (user is not null)
         {
-            // If this is a returning user, i.e., we already have a `User` with this `Identity`,
-            // set `Online: true`, but leave `Name` and `Identity` unchanged.
             user.Online = true;
-            ctx.Db.user.Identity.Update(user);
-        }
-        else
-        {
-            // If this is a new user, create a `User` object for the `Identity`,
-            // which is online, but hasn't set a name.
-            ctx.Db.user.Insert(
-                new User
-                {
-                    Name = null,
-                    Identity = ctx.Sender,
-                    Online = true,
-                }
-            );
+            user.isActive = true;
+            ctx.Db.user.Client.Update(user);
         }
     }
-
     /// <summary>
     /// This reducer will un-set the online status of the client when they disconnect.
     /// </summary>
     /// <param name="ctx"></param>
-    [Reducer(ReducerKind.ClientDisconnected)]
-    public static void ClientDisconnected(ReducerContext ctx)
+    public static void UserDisconnected(ReducerContext ctx)
     {
-        var user = ctx.Db.user.Identity.Find(ctx.Sender);
+        Log.Info($"Disconnect {ctx.Sender}");
+        var user = ctx.Db.user.Client.Find(ctx.Sender);
 
         if (user is not null)
         {
-            // This user should exist, so set `Online: false`.
             user.Online = false;
-            ctx.Db.user.Identity.Update(user);
+            user.isActive = false;
+            ctx.Db.user.Client.Update(user);
         }
-        else
+    }
+
+    public static void UpdateCursor(ReducerContext ctx, float x, float y)
+    {
+        var cursor = ctx.Db.cursor.Insert(new Cursor
         {
-            // User does not exist, log warning
-            Log.Warn("Warning: No user found for disconnected client.");
+            Client = ctx.Sender,
+            X = x,
+            Y = y,
+            LastUpdated = ctx.Timestamp
+        });
+
+        // if (ctx.Timestamp.CompareTo(cursor.LastUpdated) > 5000000)
+        {
+            // Do not draw cursor | Change field then update
+            
         }
+
     }
 
 }
